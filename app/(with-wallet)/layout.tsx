5031ce99a434b64b6f5b006ab3eeb597700ac285
mrathod05 <mrathod420@gmail.com>
2025-03-28 18:50:24 +0530

    Reset git cache and re-add files

--- app/(with-wallet)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import WalletConnectionWrapper from "@/lib/components/WalletConnectionWrapper";
-import { ReactNode } from "react";
-
-export default function RootLayout({ children }: { children: ReactNode }) {
-  return <WalletConnectionWrapper>{children}</WalletConnectionWrapper>;
-}
